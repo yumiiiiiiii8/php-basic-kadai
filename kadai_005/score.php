c830b09471b074c5f07e5285fc3046d9c23d5f65
yumiiiiiiii8 <fv-family.m@ezweb.ne.jp>
2025-12-14 22:07:22 +0900

    Add files via upload

--- kadai_005/score.php
+++ kadai_005/score.php
@@ -1,5 +1,4 @@
 <?php
-// Step1：変数を作成し、値を代入
 $score1 = 80;
 $score2 = 60;
 $score3 = 55;
@@ -11,11 +10,9 @@ $score8 = 95;
 $score9 = 30;
 $score10 = 60;
 
-// Step2：合計点と平均点を算出
 $total = $score1 + $score2 + $score3 + $score4 + $score5
        + $score6 + $score7 + $score8 + $score9 + $score10;
 
 $average = $total / 10;
 
-// 平均点をブラウザに出力
 echo $average;
